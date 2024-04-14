24044a26be4e4b93afe9b53dea662038b2087b53
0ccssu <128601631+0ccssu@users.noreply.github.com>
2024-04-14 15:27:46 +0900

    feat :: 회원 헤더 구현
    MemberHeader 구현

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NonHeader from "../utils/layout/header/NonHeader";
+import MemberHeader from "../utils/layout/header/MemberHeader";
 
 const AppRouter = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<NonHeader />} />
+                <Route path="/" element={<MemberHeader />} />
             </Routes>
         </Router>
     );
--- src/utils/layout/header/MemberHeader.jsx
+++ src/utils/layout/header/MemberHeader.jsx
@@ -1,24 +1,22 @@
 import React from "react";
-import * as D from '../../../components/styles/NonHeaderStyle';
+import * as D from '../../../components/styles/MemberHeaderStyle';
 
-
-
-const NonHeader = () => {
+const MemberHeader = () => {
     return (
         <D.Form>
             <D.Bar>
-                <D.Container>
+            <D.Container>
                     <D.LogoImg />
                 </D.Container>
                 <D.Menu to="./lectureroom">강의실</D.Menu>
                 <D.Menu to="./textbookroom">교재실</D.Menu>
-                <D.AuthContainer>
-                    <D.LoginText to="./login">로그인</D.LoginText>
-                    <D.SignupText to="./signup">회원가입</D.SignupText>
+                <D.AuthContainer to="./myprofile">
+                    <D.MemberProfile />
+                    <D.MemberName>pizza</D.MemberName>
                 </D.AuthContainer>
             </D.Bar>
         </D.Form>
     );
 }
 
-export default NonHeader;
+export default MemberHeader;
